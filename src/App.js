928e7893603360a9cff839b6e1513aad14cf197e
Carson Meiling <carmc93@gmail.com>
2020-03-16 20:24:53 -0600

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Counter from './components/Counter'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// logical component
+class App extends React.Component {
+
+  // old syntax
+  // constructor(props){
+  //   super(props)
+  //   this.state = { currentNum: 0 }
+  //   this.inc = this.inc.bind(this)
+  //   this.dec = this.dec.bind(this)
+  // }
+  // inc() {
+  //   this.setState({ currentNum: this.state.currentNum + 1 })
+  // }
+
+
+  render (){
+    return(
+      <div>
+      <h1>My Counter App</h1>
+      <Counter color='green' title='mine'/> 
+      <Counter color='orange' title='yours'/>
+      <Counter color='blue' title='his'/>
+      <Counter />
+      <Counter />
+      <Counter />
+      </div>
+    )
+  }
 }
+
 
 export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class Counter extends Component {
+  state = { currentNum: 0 }
+
+
+  inc = () => {
+    this.setState({ currentNum: this.state.currentNum + 1 })
+  }
+  dec = () => {
+    this.setState({ currentNum: this.state.currentNum - 1 })
+  }
+
+  render () {
+    const{ color, title } = this.props
+    // const color = this.props.color
+    const{ currentNum} = this.state
+    return(
+      <div style={{ backgroundColor: color }}>
+        <p>{ title }</p>
+        <p>{ currentNum }</p>
+        <button onClick = {this.inc}>+</button>
+        <button onClick = {this.dec}>-</button>
+        <hr />
+      </div>
+
+    )
+  }
+}
+
+export default Counter;
